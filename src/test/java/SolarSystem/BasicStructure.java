cb28b12a842ece39c38aeb8e58366fefee19fd26
rami-abdalla <rami.abdallaa@gmail.com>
2025-02-19 19:31:46 -0500

    basic structure of the solar system

---
+++ src/test/java/SolarSystem/BasicStructure.java
@@ -0,0 +1,10 @@
+package SolarSystem;
+
+public class BasicStructure {
+
+    int sun;
+
+    int planets;
+
+    int moon;
+}
---
+++ src/test/java/SolarSystem/SolarSystem.java
@@ -0,0 +1,18 @@
+package SolarSystem;
+
+public class SolarSystem {
+
+
+    public static void main(String[] args) {
+
+
+        BasicStructure obrey = new BasicStructure();
+
+        obrey.sun =1;
+
+        obrey.moon=2;
+
+        obrey.planets=2;
+
+    }
+}
